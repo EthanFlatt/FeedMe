1f843ad3dd0ef438457b027e86c03702ac9fabae
Ethan Flatt <ethanflatt1@gmail.com>
2023-08-02 02:01:55 -0500

    More stuff

--- FeedMe/src/components/AddDish.jsx
+++ FeedMe/src/components/AddDish.jsx
@@ -25,10 +25,15 @@ const addDish = () => {
 
     return (
         <div className="addDishPage">
+            <h3>Name</h3>
             <input className="addDishNameBar" type='text' value={formState.name} id='name' onChange={handleChange}></input>
+            <h3>Ingredients</h3>
             <input className="addDishIngredientsBar" type='text' value={formState.ingredients} id='ingredients' onChange={handleChange}></input>
+            <h3>Instructions</h3>
             <input className="addDishInstructionsBar" type='text' value={formState.instructions} id='instructions' onChange={handleChange}></input>
+            <h3>Photo</h3>
             <input className="addDishPhotoBar" type='text' value={formState.photo} id='photo' onChange={handleChange}></input>
+            <h3>Country</h3>
             <input className="addDishCountryBar" type='text' value={formState.country} id='country' onChange={handleChange}></input>
             <button className='addDishButton' onClick={handleSubmit}>Submit</button>
         </div>
